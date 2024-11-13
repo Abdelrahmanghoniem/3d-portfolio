286b8212070492d637a40922e6cead9a3ffe6065
Abdelrahmanghoniem <116071431+Abdelrahmanghoniem@users.noreply.github.com>
2024-11-13 22:37:40 +0200

    fixes in mobile computers view

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -72,14 +72,29 @@ const ComputersCanvas = () => {
   camera={{position:[20,3,5],fov:25}}
   gl={{ preserveDrawingBuffer: true, powerPreference: "high-performance", antialias: true, version: 1 }}
   >
-    <Suspense fallback={<CanvasLoader/>}>
+      <Suspense fallback={<CanvasLoader />}>
     <OrbitControls
-    enableZoom={false}
-    maxPolarAngle={Math.PI/2}
-    minPolarAngle={Math.PI/2}
-    /> 
-    <Computers isMobile={isMobile}/>
-    </Suspense>
+      enableZoom={false}
+      maxPolarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 2}
+    />
+    <hemisphereLight intensity={1.65} groundColor="black" />
+    
+    {!isMobile && (
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+    )}
+
+    <pointLight intensity={0.3} />
+    <Computers isMobile={isMobile} />
+  </Suspense>
+
     <Preload all/>
     
   </Canvas>
