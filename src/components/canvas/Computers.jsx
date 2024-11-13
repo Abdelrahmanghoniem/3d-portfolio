f66d6b0db0f355247cd26d12cc614bfc864824aa
Abdelrahmanghoniem <116071431+Abdelrahmanghoniem@users.noreply.github.com>
2024-11-13 23:01:11 +0200

    slight changes in the computers canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,105 +1,74 @@
 /* eslint-disable react/no-unknown-property */
-import { Suspense,useEffect,useState } from "react"
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls,Preload,useGLTF } from "@react-three/drei"
-import CanvasLoader from "../Loader"
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
 // eslint-disable-next-line react/prop-types
-const Computers = ({isMobile}) => {
-  
-  const computer =useGLTF('./desktop_pc/scene.gltf');
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+
   return (
     <mesh>
-      <hemisphereLight intensity={1.65}
-       groundColor="black" />
-      
-      <pointLight intensity={0.30}/>
-      
-      <spotLight
-        position={[-20,50,10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
+      <hemisphereLight intensity={1.65} groundColor="black" />
+      <pointLight intensity={0.30} />
+
+      {/* Conditionally render or adjust spotLight for non-mobile */}
+      {!isMobile && (
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+      )}
 
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.5 : 0.75}
         position={isMobile ? [0, -2.5, -1.5] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
-      />      
-
-      
-      
-
-
-
+      />
     </mesh>
-  )
-}
-
-
+  );
+};
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the `isMobile` state variable
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
     setIsMobile(mediaQuery.matches);
 
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
+    return () => mediaQuery.removeEventListener("change", handleMediaQueryChange);
   }, []);
 
-  return(
-  <Canvas
-  frameloop="demand"
-  shadows={!isMobile} // Disable shadows on mobile
-  dpr={[1,2]}
-  camera={{position:[20,3,5],fov:25}}
-  gl={{ preserveDrawingBuffer: true, powerPreference: "high-performance", antialias: true, version: 1 }}
-  >
+  return (
+    <Canvas
+      frameloop={isMobile ? "demand" : "always"} // Reduce render loop on mobile
+      shadows={!isMobile} // Disable shadows on mobile
+      dpr={[1, isMobile ? 1.5 : 2]} // Reduce pixel ratio on mobile
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true, powerPreference: "high-performance" }}
+    >
       <Suspense fallback={<CanvasLoader />}>
-    <OrbitControls
-      enableZoom={false}
-      maxPolarAngle={Math.PI / 2}
-      minPolarAngle={Math.PI / 2}
-    />
-    <hemisphereLight intensity={1.65} groundColor="black" />
-    
-    {!isMobile && (
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-    )}
-
-    <pointLight intensity={0.3} />
-    <Computers isMobile={isMobile} />
-  </Suspense>
-
-    <Preload all/>
-    
-  </Canvas>
-  
-  )   
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
 };
 
-export default ComputersCanvas  
+export default ComputersCanvas;
